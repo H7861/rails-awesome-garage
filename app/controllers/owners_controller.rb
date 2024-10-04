4909536e8441cab4a3d4e2e69ded14192c02aa75
hamza khalid <kingkhanibf@gmail.com>
2024-10-04 09:47:32 +0100

    updates made

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,10 @@
 class OwnersController < ApplicationController
+  def index
+    @owners = Owner.all  # Get all the owners
+  end
+
+  def show
+    @owner = Owner.find(params[:id])  # Find a specific owner by ID
+    @cars = @owner.cars  # Get all the cars owned by this owner
+  end
 end
